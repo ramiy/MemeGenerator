9deae87d6f1f0031d8605e3a0c139cf6d525737a
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-06-20 14:19:05 +0300

    initial release

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gImgs = [{ id: 1, url: 'img/popo.jpg', keywords: ['happy'] }];
+var gMeme = {
+	selectedImgId: 5,
+	txts: [
+		{
+			line: 'I never eat Falafel',
+			size: 20,
+			align: 'left',
+			color: 'red'
+		}
+	]
+}

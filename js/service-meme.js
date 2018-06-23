4074e01d6da9a7f1bc90eccefd889e5256824d07
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-06-23 19:31:24 +0300

    Meme curent text index

--- js/service-meme.js
+++ js/service-meme.js
@@ -21,6 +21,7 @@ var gMeme = {
         },
     ]
 }
+var gCurrTextIdx = 0;
 
 
 
@@ -38,32 +39,32 @@ function getMemeImage() {
 
 // Retrieve meme text
 function getMemeText() {
-    return gMeme.txts[0].text;
+    return gMeme.txts[gCurrTextIdx].text;
 }
 
 // Retrieve meme font
 function getMemeFont() {
-    return gMeme.txts[0].font;
+    return gMeme.txts[gCurrTextIdx].font;
 }
 
 // Retrieve meme color
 function getMemeColor() {
-    return gMeme.txts[0].color;
+    return gMeme.txts[gCurrTextIdx].color;
 }
 
 // Retrieve meme size
 function getMemeSize() {
-    return gMeme.txts[0].size;
+    return gMeme.txts[gCurrTextIdx].size;
 }
 
 // Retrieve meme position X
 function getMemePositionX() {
-    return gMeme.txts[0].positionX;
+    return gMeme.txts[gCurrTextIdx].positionX;
 }
 
 // Retrieve meme position Y
 function getMemePositionY() {
-    return gMeme.txts[0].positionY;
+    return gMeme.txts[gCurrTextIdx].positionY;
 }
 
 /*************** UPDATE ***************/
@@ -75,45 +76,45 @@ function updateMemeImage(imgId) {
 
 // Update the meme text
 function updateMemeText(text) {
-    gMeme.txts[0].text = text;
+    gMeme.txts[gCurrTextIdx].text = text;
 }
 
 // Update the meme font
 function updateMemeFont(font) {
-    gMeme.txts[0].font = font;
+    gMeme.txts[gCurrTextIdx].font = font;
 }
 
 // Update the meme color
 function updateMemeColor(color) {
-    gMeme.txts[0].color = color;
+    gMeme.txts[gCurrTextIdx].color = color;
 }
 
 // Update the meme size
 function updateMemeSize(size) {
-    gMeme.txts[0].size += size;
+    gMeme.txts[gCurrTextIdx].size += size;
 }
 
 // Update the meme position X
 function updateMemePositionX(position) {
-    gMeme.txts[0].positionX += position;
+    gMeme.txts[gCurrTextIdx].positionX += position;
 }
 
 // Update the meme position Y
 function updateMemePositionY(position) {
-    gMeme.txts[0].positionY += position;
+    gMeme.txts[gCurrTextIdx].positionY += position;
 }
 
 // Update the meme alignment
 function updateMemeAlignment(position, canvasWidth, textWidth) {
     switch (position) {
         case -1:
-            gMeme.txts[0].positionX = 0;
+            gMeme.txts[gCurrTextIdx].positionX = 0;
             break;
         case 0:
-            gMeme.txts[0].positionX = (canvasWidth - textWidth) / 2;
+            gMeme.txts[gCurrTextIdx].positionX = (canvasWidth - textWidth) / 2;
             break;
         case 1:
-            gMeme.txts[0].positionX = canvasWidth - textWidth;
+            gMeme.txts[gCurrTextIdx].positionX = canvasWidth - textWidth;
             break;
     }
 }
